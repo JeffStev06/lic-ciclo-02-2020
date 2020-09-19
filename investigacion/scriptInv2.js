68c2712a9476e0d5b477f21a24ab81cff4afbb7b
JeffStev06 <jeffrey.stv06@gmail.com>
2020-09-19 12:45:01 -0600

    investigacion2 terminada

---
+++ investigacion/scriptInv2.js
@@ -0,0 +1,29 @@
+
+var array = []
+var num = 0;
+var cuadros = ""
+
+function testPush() {
+    num += 1
+
+    array.push(num)
+
+    pintar();
+}
+
+function testPop() {
+    
+    array.pop()
+
+    pintar();
+}
+
+function pintar() {
+    cuadros = ""
+    array.forEach(function(num) {
+        cuadros = cuadros +
+            '<div class="cuadro">'+ num +'</div>'
+    }) 
+
+    document.getElementById('cuadros').innerHTML = cuadros;
+}
